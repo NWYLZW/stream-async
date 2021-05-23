c8d19d13463de82ac858a0b5631639003a00df45
yijie4188 <yijie4188@gmail.com>
2021-05-23 17:27:16 +0800

    feat(test): perfect unit test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,23 +1,8 @@
-import { use } from 'chai'
+import { expect, use } from 'chai'
 import { streamAsync } from 'stream-async'
 import cap from 'chai-as-promised'
 
 use(cap)
-
-const logger = t => new Proxy(t, {
-  get(target, p) {
-    console.log(
-      'logger.get:', target, p
-    )
-    return t[p]
-  },
-  apply(target, thisArg, argArray) {
-    console.log(
-      'logger.apply:', target, thisArg, argArray
-    )
-    return Reflect.apply(target, thisArg, argArray)
-  }
-})
 
 describe('basic support', function () {
   class A {
@@ -36,19 +21,11 @@ describe('basic support', function () {
     }
   }
   it('should await fun by chain.', async () => {
-    const a = new A(), x = Promise.resolve(a)
-    console.log(
-      await x.then(
-        (...args) => x.then(a => a.asyncFun.apply(a, args))
-      ).then(
-        (...args) => x.then(a => a.asyncFun.apply(a, args))
-      ).then(
-        a => a.foo
-      )
-    )
-
-    console.log(
-      await a.stream.fun().asyncFun()
-    )
+    const a = new A()
+    await a.stream
+      .fun()
+      .asyncFun()
+      .fun()
+    expect(a.foo).to.eq(4)
   })
 })
